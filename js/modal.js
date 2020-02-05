e0064211c715d2123733370c29382c7dda2f29e6
RizlaDNB <44847886+RizlaDNB@users.noreply.github.com>
2020-02-05 19:01:16 +0200

    modal edit
    html. new input name,
    js. new vars

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,29 @@
 var modal = document.querySelector('.modal-booking'),
-    modalOverlay = document.querySelector('.modal-overlay');
+    modalOverlay = document.querySelector('.modal-overlay'),
+    ownerName = document.querySelector('[name=owner-name]'),
+    petName = document.querySelector('[name=pet-name]'),
+    phoneNumber = document.querySelector('[name=phone-number]'),
+    email = document.querySelector('[name=email]'),
+    fromDate = document.querySelector('[name=from-date]'),
+    toDate = document.querySelector('[name=to-date]'),
+    formError = modal.querySelector('form');
 
 $('.modal-open').on('click', function (evt) {
     evt.preventDefault();
     modal.style.display = "block";
     modalOverlay.style.display = "block";
+    ownerName.focus();
 });
 
 $('.modal-close').on('click', function (evt) {
     evt.preventDefault();
     modal.style.display = "none";
     modalOverlay.style.display = "none";
+});
+
+$('form').on('submit', function(evt){
+    if (!ownerName || !petName || !phoneNumber || !email || !fromDate || !toDate) {
+        evt.preventDefault();
+        console.log('Заполните все поля');
+    }
 });
