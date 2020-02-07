ea2ceb38fc462508914877c542a1362c230ae84f
RizlaDNB <44847886+RizlaDNB@users.noreply.github.com>
2020-02-07 20:08:52 +0200

    p2
    Fonts edited,
    styles edited,
    new page added

---
+++ js/slider-setting.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $('.section-rooms-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<button class="prev-rooms-arrow arrow"></button>',
+        nextArrow: '<button class="next-rooms-arrow arrow"></button>',
+        dots: true,
+        appendArrows: '.section-rooms-slider',
+        appendDots: '.section-rooms-slider'
+    });
+});
+
+$(document).ready(function(){
+    $('.section-feedback-slider').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        prevArrow: '<button class="prev-feedback-arrow arrow"></button>',
+        nextArrow: '<button class="next-feedback-arrow arrow"></button>',
+        dots: true,
+        dotsClass: 'feedback-dots',
+        centerMode: true,
+        variableWidth: true,
+        appendArrows: '.section-feedback-slider',
+        appendDots: '.section-feedback-slider'
+    });
+});
