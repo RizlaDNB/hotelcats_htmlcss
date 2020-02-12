50775c5d2b6e2c730a2bc62726c7031902c51ab2
RizlaDNB <44847886+RizlaDNB@users.noreply.github.com>
2020-02-12 21:18:35 +0200

    Вторая модалка+немного js

--- js/modal.js
+++ js/modal.js
@@ -8,7 +8,10 @@ var modal = document.querySelector('.modal-booking'),
     email = document.querySelector('[name=email]'),
     fromDate = document.querySelector('[name=from-date]'),
     toDate = document.querySelector('[name=to-date]'),
-    formError = modal.querySelector('form');
+    formError = modal.querySelector('form'),
+    modalSuccefful = document.querySelector('.modal-booking-successful'),
+    modalSucceffulClose = document.querySelector('.button-modal-booking-successful'),
+    modalSend = document.querySelector('.modal-send-button'); //Убрать позднее
 
 //Открытие модального окна с любой кнопки
 for (var i = 0; i < modalOpen.length; i++) {
@@ -20,11 +23,12 @@ for (var i = 0; i < modalOpen.length; i++) {
         })  
 };
 
-//Закрытие модалки по кнопке
+//Закрытие модалки по крестику
 modalClose.addEventListener('click', function(evt) {
     evt.preventDefault();
     modal.classList.remove('modal-show');
     modalOverlay.classList.remove('modal-show');
+    modalSuccefful.classList.remove('modal-show');
 });
 
 //Закрытие модалки по клику в любую область, кроме модалки
@@ -32,12 +36,27 @@ modalOverlay.addEventListener('click', function(evt) {
     evt.preventDefault();
     modal.classList.remove('modal-show');
     modalOverlay.classList.remove('modal-show');
+    modalSuccefful.classList.remove('modal-show');
 });
 
-//TODO Алерт, в случае если ничего не введено в форму
-formError.addEventListener('submit', function(evt) {
-    if (!ownerName || !petName || !phoneNumber || !email || !fromDate || !toDate) {
-        evt.preventDefault();
-        console.log('Заполните все поля');
-    }
+//Закрытие модалок по клику на кнопку "ОК"
+modalSucceffulClose.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    modal.classList.remove('modal-show');
+    modalOverlay.classList.remove('modal-show');
+    modalSuccefful.classList.remove('modal-show');
 });
+
+//Открытие модалки подтверждения после клика на кнопку отправить TODO убрать
+modalSend.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    modalSuccefful.classList.add('modal-show');
+});
+
+//TODO Алерт, в случае если не все поля формы заполнены
+// formError.addEventListener('submit', function(evt) {
+//     if (!ownerName || !petName || !phoneNumber || !email || !fromDate || !toDate) {
+//         evt.preventDefault();
+//         console.log('Заполните все поля');
+//     }
+// });
