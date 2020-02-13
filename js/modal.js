e08a055d74637b4705063c1787a4278706421d7c
RizlaDNB <44847886+RizlaDNB@users.noreply.github.com>
2020-02-13 17:58:53 +0200

    исправление относительных ссылок в каталоге. работа с js

--- js/modal.js
+++ js/modal.js
@@ -2,16 +2,17 @@ var modal = document.querySelector('.modal-booking'),
     modalOverlay = document.querySelector('.modal-overlay'),
     modalOpen = document.getElementsByClassName('modal-open-button'),
     modalClose = document.querySelector('.modal-close-button'),
-    ownerName = document.querySelector('[name=owner-name]'),
-    petName = document.querySelector('[name=pet-name]'),
-    phoneNumber = document.querySelector('[name=phone-number]'),
-    email = document.querySelector('[name=email]'),
-    fromDate = document.querySelector('[name=from-date]'),
-    toDate = document.querySelector('[name=to-date]'),
-    formError = modal.querySelector('form'),
     modalSuccefful = document.querySelector('.modal-booking-successful'),
-    modalSucceffulClose = document.querySelector('.button-modal-booking-successful'),
-    modalSend = document.querySelector('.modal-send-button'); //Убрать позднее
+    modalSucceffulBtn = document.querySelector('.button-modal-booking-successful'),
+    modalSend = document.querySelector('.modal-send-button'), //Убрать позднее
+    form = modal.querySelector('form'),
+    ownerName = form.querySelector('[name=owner-name]'),
+    petName = form.querySelector('[name=pet-name]'),
+    phoneNumber = form.querySelector('[name=phone-number]'),
+    email = form.querySelector('[name=email]'),
+    fromDate = form.querySelector('[name=from-date]'),
+    toDate = form.querySelector('[name=to-date]');
+
 
 //Открытие модального окна с любой кнопки
 for (var i = 0; i < modalOpen.length; i++) {
@@ -23,6 +24,7 @@ for (var i = 0; i < modalOpen.length; i++) {
         })  
 };
 
+
 //Закрытие модалки по крестику
 modalClose.addEventListener('click', function(evt) {
     evt.preventDefault();
@@ -31,7 +33,8 @@ modalClose.addEventListener('click', function(evt) {
     modalSuccefful.classList.remove('modal-show');
 });
 
-//Закрытие модалки по клику в любую область, кроме модалки
+
+//Закрытие модалки по клику в оверлей
 modalOverlay.addEventListener('click', function(evt) {
     evt.preventDefault();
     modal.classList.remove('modal-show');
@@ -39,24 +42,24 @@ modalOverlay.addEventListener('click', function(evt) {
     modalSuccefful.classList.remove('modal-show');
 });
 
-//Закрытие модалок по клику на кнопку "ОК"
-modalSucceffulClose.addEventListener('click', function(evt) {
+
+//Закрытие модалки по клику на кнопку "ОК"
+modalSucceffulCloseBtn.addEventListener('click', function(evt) {
     evt.preventDefault();
     modal.classList.remove('modal-show');
     modalOverlay.classList.remove('modal-show');
     modalSuccefful.classList.remove('modal-show');
 });
 
-//Открытие модалки подтверждения после клика на кнопку отправить TODO убрать
-modalSend.addEventListener('click', function(evt) {
-    evt.preventDefault();
-    modalSuccefful.classList.add('modal-show');
-});
 
 //TODO Алерт, в случае если не все поля формы заполнены
-// formError.addEventListener('submit', function(evt) {
-//     if (!ownerName || !petName || !phoneNumber || !email || !fromDate || !toDate) {
-//         evt.preventDefault();
-//         console.log('Заполните все поля');
-//     }
-// });
+form.addEventListener('submit', function(evt) {
+    evt.preventDefault();
+    console.log('Клик по кнопке');
+    console.log('Имя клиента:');
+    console.log('Имя питомца:');
+    console.log('Телефон:');
+    console.log('E-mail:');
+    console.log('Дата заезда:');
+    console.log('Дата выезда:');
+});
